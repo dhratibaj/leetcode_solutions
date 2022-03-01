e9df202a4a02c5d5b4ae017cca00d5a2011a4de5
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-03-01 15:35:11 +0530

    Time: 867 ms (28.07%), Space: 15.3 MB (29.44%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,10 +1,23 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        gap = 0
-        for i in range(len(nums) - 2, -1, -1):
-            gap = 0 if nums[i] > gap else (gap + 1)
-        return gap is 0
+        n = len(nums)
+        if n <= 1: return True
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            if dp[i-1] < i: 
+                dp[i] = dp[i-1] 
+            else:
+                dp[i] = max(dp[i-1], nums[i]+i) 
+        return dp[-1] >= n-1
         
+        #----------------------------------------------
+        # gap = 0
+        # for i in range(len(nums) - 2, -1, -1):
+        #     gap = 0 if nums[i] > gap else (gap + 1)
+        # return gap is 0
+        
+        #-----------------------------------------------------
         # #''' Greedy Soln (Explore from End -> Start) '''
         # size = len(nums)
         # goal = size - 1 
