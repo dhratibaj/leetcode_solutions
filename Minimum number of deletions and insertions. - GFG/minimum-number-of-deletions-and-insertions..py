33d17cb5d3951ea2893cd41d2a7513c60e62d450
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-03-03 16:06:06 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..py
@@ -0,0 +1,32 @@
+#User function Template for python3
+class Solution:
+	def minOperations(self, s1, s2):
+	    def lcs(s1, s2):
+	        n,m = len(s1),len(s2)
+	        dp = [[-1 for i in range(m+1)]for i in range(n+1)]
+	        for i in range(n+1):
+	            dp[i][0] = 0
+	        for j in range(m+1):
+	            dp[0][j] = 0
+	        for i in range(1,n+1):
+	            for j in range(1,m+1):
+	                if s1[i-1] == s2[j-1]:
+	                    dp[i][j] = 1 + dp[i-1][j-1]
+	                else:
+	                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+	        return dp[-1][-1]
+	    x = lcs(s1,s2)
+	    return (len(s1)-x) + (len(s2)-x)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s1,s2 = input().split()
+		ob = Solution()
+		ans = ob.minOperations(s1,s2)
+		print(ans)
+# } Driver Code Ends
