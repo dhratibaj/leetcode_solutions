45457b436b19b802dcfe319465e2a322db64386a
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-03-06 22:51:26 +0530

    Time: 51 ms (52.27%), Space: 13.9 MB (88.07%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,6 +1,9 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        if n == 1:
-            return 1
-        else:
-            return (n*(2*n-1)*self.countOrders(n-1))%(10**9+7)
+        return (math.factorial(n * 2) >> n) % (10**9 + 7)
+        
+        #------------------------------------------------------------
+        # if n == 1:
+        #     return 1
+        # else:
+        #     return (n*(2*n-1)*self.countOrders(n-1))%(10**9+7)
