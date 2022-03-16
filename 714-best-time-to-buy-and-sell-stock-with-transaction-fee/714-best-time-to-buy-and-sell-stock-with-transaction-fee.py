56b9c287896f38ca2399546aebd5925cbe236036
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-03-16 13:52:48 +0530

    Time: 713 ms (91.99%), Space: 21.4 MB (59.89%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,22 +1,35 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        memo = {}
-        def dp(i, buy):
-            if i == len(prices):
-                return 0
+        #---------------------Greedy---------------------------
+        profit, curr_min = 0, float("inf")
+		
+        for p in prices:
+            curr_min = min(curr_min, p)
             
-            key = (i, buy)
-            if key in memo:
-                return memo[key]
-            
-            profit = dp(i+1, buy)
-            if buy:
-                profit = max(profit, dp(i+1, not buy) - prices[i])
-            else:
-                # If selling then also consider the transaction fee of selling
-                profit = max(profit, dp(i+1, not buy) + prices[i] - fee)
-            
-            memo[key] = profit 
-            return profit
+            if p - curr_min - fee > 0:
+                profit += (p - curr_min - fee)
+                curr_min = p - fee
         
-        return dp(0, True)
+        return profit
+        
+        #--------------DP----------------------------------------
+#         memo = {}
+#         def dp(i, buy):
+#             if i == len(prices):
+#                 return 0
+            
+#             key = (i, buy)
+#             if key in memo:
+#                 return memo[key]
+            
+#             profit = dp(i+1, buy)
+#             if buy:
+#                 profit = max(profit, dp(i+1, not buy) - prices[i])
+#             else:
+#                 # If selling then also consider the transaction fee of selling
+#                 profit = max(profit, dp(i+1, not buy) + prices[i] - fee)
+            
+#             memo[key] = profit 
+#             return profit
+        
+#         return dp(0, True)
