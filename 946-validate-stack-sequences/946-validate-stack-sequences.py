9f22b5413047102314d972d3225b4837228ef8b2
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-03-16 11:24:28 +0530

    Time: 82 ms (74.81%), Space: 14.1 MB (93.97%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        j = 0
+        for e in pushed:
+            stack.append(e)
+            while stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+        return j == len(popped) 
