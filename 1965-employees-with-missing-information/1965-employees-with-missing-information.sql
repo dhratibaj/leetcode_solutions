f36e5bd8af0b0a689cd1ace52994874318720529
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-04-13 23:40:49 +0530

    Time: 548 ms (44.96%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,7 +1,8 @@
-select employee_id from(
-select employee_id, name, null as salary from Employees
-union all
-select employee_id, null as name, salary from Salaries)
-group by employee_id
-having count(*) <= 1
-order by employee_id
+SELECT employee_id 
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+UNION
+SELECT employee_id 
+FROM salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id;
