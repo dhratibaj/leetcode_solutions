657fae18da49c4eed640164cd75e7dd6e1bee82c
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-07-15 03:09:34 +0530

    Time: 53 ms (69.33%), Space: 14 MB (57.64%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,18 +1,35 @@
 class Solution:
-    def func(self,a,arr,res,freq):
-        if len(arr) == len(a):
-            res.append(list(arr))
+    def func(self,idx,a,res):
+        if idx == len(a):
+            res.append(list(a))
             return
-        for i in range(len(a)):
-            if (not freq[i]):
-                arr.append(a[i])
-                freq[i] = 1
-                self.func(a,arr,res,freq)
-                freq[i] = 0
-                arr.pop()
+        for i in range(idx,len(a)):
+            a[idx],a[i]=a[i],a[idx]
+            self.func(idx+1,a,res)
+            a[idx],a[i]=a[i],a[idx]
     
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        freq = [0]*len(nums)
-        self.func(nums,[],res,freq)
+        self.func(0,nums,res)
         return res
+        
+        
+#-----------------------Approach 2----------------------------
+# class Solution:
+#     def func(self,a,arr,res,freq):
+#         if len(arr) == len(a):
+#             res.append(list(arr))
+#             return
+#         for i in range(len(a)):
+#             if (not freq[i]):
+#                 arr.append(a[i])
+#                 freq[i] = 1
+#                 self.func(a,arr,res,freq)
+#                 freq[i] = 0
+#                 arr.pop()
+    
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         freq = [0]*len(nums)
+#         self.func(nums,[],res,freq)
+#         return res
