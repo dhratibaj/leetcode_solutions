b2035faf15f2ac7c52750cc540c9fe67841f8ffb
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-07-21 10:42:26 +0530

    Time: 79 ms (11.23%), Space: 13.9 MB (32.33%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -3,21 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+
+#-------------------------------------ITERATIVE------------------------------------------------------------------
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode()
-        dummy_node = head
-        while list1 and list2:
-            if list1.val < list2.val:
-                head.next = ListNode(list1.val)
-                head = head.next
+        if list1 == None: return list2
+        if list2 == None: return list1
+        dummy = ListNode(-101)
+        head = dummy
+        while(list1!=None and list2!=None):
+            if(list1.val<list2.val):
+                newnode = ListNode(list1.val)
+                dummy.next = newnode
                 list1 = list1.next
             else:
-                head.next = ListNode(list2.val)
-                head = head.next
-                list2 = list2.next     
-        if list1:
-            head.next = list1
-        if list2:
-            head.next = list2  
-        return dummy_node.next
+                newnode = ListNode(list2.val)
+                dummy.next = newnode
+                list2 = list2.next
+            dummy = dummy.next
+        if list1!=None: dummy.next = list1
+        if list2!=None: dummy.next = list2
+        return head.next
