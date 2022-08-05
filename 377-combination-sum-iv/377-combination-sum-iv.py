cbc8d65d3c4b0d6160fba8c51ffb84836cc59d8d
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-05 09:36:03 +0530

    Time: 45 ms (87.05%), Space: 14.3 MB (10.06%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,14 +1,8 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        #Initialize list 
-        dp = [0] * (target + 1)
-		#Set base case
-        dp[0] = 1
-		#Iterate thru each possible sum up to target
-        for total in range(1, target + 1):
-			#For each number in input list, if in bounds add number of combinations up to (total - num)	
-            for num in nums:
-                if total - num >= 0:
-                    dp[total] += dp[total - num]
-		#Return number of combinations up to target
-        return dp[target]
+        @cache
+        def dp(target):
+            if target == 0: return 1
+            if target < 0: return 0
+            return sum(dp(target - num) if target >= num else 0 for num in nums)
+        return dp(target)
