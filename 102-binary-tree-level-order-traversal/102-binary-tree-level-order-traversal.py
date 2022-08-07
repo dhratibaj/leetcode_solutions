15bdd85bf2c4f1c178fff3d1b4d46f89c20d3917
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-08 00:19:24 +0530

    Time: 38 ms (89.82%), Space: 14.2 MB (85.08%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -5,14 +5,40 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root, level, res):
-        if not root: return 
-        if len(res) < level+1: res.append([])
-        res[level].append(root.val)
-        self.dfs(root.left, level+1, res)
-        self.dfs(root.right, level+1, res)
-    
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        self.dfs(root, 0, res)
+        from collections import deque
+        deque, res = deque(), []
+        if root:
+            deque.append(root)
+        while deque:
+            level, size = [], len(deque)
+            for _ in range(size):
+                node = deque.popleft()
+                level.append(node.val)
+                if node.left:
+                    deque.append(node.left)
+                if node.right:
+                    deque.append(node.right)
+            res.append(level)
         return res
+    
+    
+#-------------------------------python recursive approach------------------------------------------
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def dfs(self, root, level, res):
+#         if not root: return 
+#         if len(res) < level+1: res.append([])
+#         res[level].append(root.val)
+#         self.dfs(root.left, level+1, res)
+#         self.dfs(root.right, level+1, res)
+    
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         res = []
+#         self.dfs(root, 0, res)
+#         return res
