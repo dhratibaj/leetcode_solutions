094af7d0ca7be5e0ce00e35d4704bd8a67673a82
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-15 13:07:10 +0530

    Time: 19 ms (17.09%), Space: 17.6 MB (25.39%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -9,23 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     int sumRootToLeaf(TreeNode* root) {
-        
-//     }
-// };
-
 class Solution {
-    int sumRootToLeaf(TreeNode* root, int sum){
-        if(!root) return 0;
-        sum = (2 * sum) + root->val;
-        if(!root->left && !root->right) return sum;
-        return sumRootToLeaf(root->left, sum) + sumRootToLeaf(root->right, sum);
-    }
 public:
-    int sumRootToLeaf(TreeNode* root) {
-        return sumRootToLeaf(root, 0);
+    void rootToLeaf(TreeNode* root, string s, int* ans){
+        if(!root->left && !root->right){
+            s += to_string(root->val);
+            ans[0]+=stoi(s,0,2);
+            return;
+        }
+        string curr = to_string(root->val);
+        if(root->left) rootToLeaf(root->left,s+curr,ans);
+        if(root->right) rootToLeaf(root->right,s+curr,ans);
     }
     
+    int sumRootToLeaf(TreeNode* root) {
+        int* ans = new int[1];
+        ans[0] = 0;
+        rootToLeaf(root,"",ans);
+        return ans[0];
+    }
 };
+
+//--------------Another approach-------------------------------------------------------------
+// class Solution {
+//     int sumRootToLeaf(TreeNode* root, int sum){
+//         if(!root) return 0;
+//         sum = (2 * sum) + root->val;
+//         if(!root->left && !root->right) return sum;
+//         return sumRootToLeaf(root->left, sum) + sumRootToLeaf(root->right, sum);
+//     }
+// public:
+//     int sumRootToLeaf(TreeNode* root) {
+//         return sumRootToLeaf(root, 0);
+//     }
+    
+// };
