50f1123256dbfb6e0ca64db4b407c002d5e7c10c
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-18 10:25:44 +0530

    Time: 71 ms (14.89%), Space: 31 MB (20.78%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,8 +11,34 @@
  */
 class Solution {
 public:
+    int findLHgt(TreeNode* root){
+        int hght = 0;
+        while(root){
+            hght ++;
+            root = root->left;
+        }
+        return hght;
+    }
+    
+    int findRHgt(TreeNode* root){
+        int hght = 0;
+        while(root){
+            hght ++;
+            root = root->right;
+        }
+        return hght;
+    }
+    
     int countNodes(TreeNode* root) {
         if(!root) return 0;
+        int lh = findLHgt(root->left);
+        int rh = findRHgt(root->right);
+        if(lh==rh) return(1<<(lh+1))-1;
         return 1+countNodes(root->left)+countNodes(root->right);
+                
+                
+        //--------------has O(n) complexity---------------------------
+        // if(!root) return 0;
+        // return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
