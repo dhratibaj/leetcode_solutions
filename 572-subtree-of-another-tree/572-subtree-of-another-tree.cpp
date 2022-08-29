a51b054c842c43eb57a5063fa9e08f9ef4750685
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-29 23:35:23 +0530

    Time: 36 ms (63.02%), Space: 29.4 MB (5.85%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -9,22 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Better solution is to only compare nodes in s with the same max depth as t. First get max depth of t, then recursively check each node in s, if depth equals, push to a vector. Then compare each node in the vector with t.
 class Solution {
+    vector<TreeNode*> nodes;
+
 public:
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(!root) return false;
-        return isSameTree(root,subRoot) || isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if (!s && !t) return true;
+        if (!s || !t) return false;
+        getDepth(s, getDepth(t, -1));
+        for (TreeNode* n: nodes)
+            if (identical(n, t))
+                return true;
+        return false;
     }
-    
-    //Leetcode 100
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL)
-            return true;
-        if(p==NULL || q==NULL)
-            return false;
-        if(p->val == q->val)
-            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-        else
-            return false;
+
+    int getDepth(TreeNode* r, int d) {
+        if (!r)
+            return -1;
+        int depth = max(getDepth(r->left, d), getDepth(r->right, d)) + 1;
+        // Check if depth equals required value
+        // Require depth is -1 for tree t (only return the depth, no push)
+        if (depth == d)
+            nodes.push_back(r);
+        return depth;
+    }
+
+    bool identical(TreeNode* a, TreeNode* b) {
+        if (!a && !b) return true;
+        if (!a || !b || a->val != b->val) return false;
+        return identical(a->left, b->left) && identical(a->right, b->right);
     }
 };
