ebd4aa0c7798ba7e0530277fcd7beea9ceb3259f
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-30 10:16:47 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (75.11%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,21 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//-------------------MORRIS INORDER Traversal--------------------------------------------------
 class Solution {
 public:
-    void Inorder(TreeNode* root, vector<int> &ans)
-    {
-        if(root == NULL) 
-            return;
-        Inorder(root->left,ans);
-        ans.push_back(root->val);
-        Inorder(root->right,ans);
-        return;
-    }
-    
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        Inorder(root,ans);
-        return ans;
+        vector<int> inorder; TreeNode *cur = root;
+        while(cur){
+            if(cur->left==NULL){
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            }
+            else{
+                TreeNode *prev = cur->left;
+                while(prev->right && prev->right!=cur)
+                    prev = prev->right;
+                if(prev->right==NULL){
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else{
+                    prev->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
+// -------------------------Normal Recursion------------------------------
+// class Solution {
+// public:
+//     void Inorder(TreeNode* root, vector<int> &ans)
+//     {
+//         if(root == NULL) 
+//             return;
+//         Inorder(root->left,ans);
+//         ans.push_back(root->val);
+//         Inorder(root->right,ans);
+//         return;
+//     }
+    
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         Inorder(root,ans);
+//         return ans;
+//     }
+// };
