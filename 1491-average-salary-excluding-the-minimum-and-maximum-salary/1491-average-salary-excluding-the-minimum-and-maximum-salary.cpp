cd468bd8652e6188001717410c67ca3e7b3eefe4
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-30 23:36:28 +0530

    Time: 3 ms (52.08%), Space: 7.2 MB (42.20%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,17 +1,8 @@
-class Solution:
-    def average(self, salary: List[int]) -> float:
-        salary.remove(min(salary))
-        salary.remove(max(salary))
-        return sum(salary)/len(salary)
-    
-        # salary.sort()
-        # s = sum(salary)-salary[0]-salary[-1]
-        # return s/(len(salary)-2)
-        
-        #-------------------------------------------
-        # count,n = 0,0
-        # for i in salary:
-        #     if i!=min(salary) and i!=max(salary):
-        #         count+=(i)
-        #         n+=1
-        # return count/n
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int mi=INT_MAX,ma=INT_MIN,s=0;
+        for(auto i:salary)mi=min(mi,i),ma=max(ma,i),s+=i;
+        return (s-ma-mi)*1.0/(salary.size()-2);
+    }
+};
