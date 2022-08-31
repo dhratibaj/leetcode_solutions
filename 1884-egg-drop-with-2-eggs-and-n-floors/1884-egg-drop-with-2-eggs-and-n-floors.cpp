e9fc4aafff43649854beb7b277130a72dd6cb380
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-08-31 12:25:38 +0530

    Time: 2 ms (82.21%), Space: 5.8 MB (90.05%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,10 +1,6 @@
-int dp[1001]={};
 class Solution {
 public:
     int twoEggDrop(int n) {
-        if (dp[n] == 0)
-		for (int i = 1; i <= n; ++i)
-			dp[n] = min(dp[n] == 0 ? n : dp[n], 1 + max(i - 1, twoEggDrop(n - i)));
-	    return dp[n];
+        return (int)ceil((-1.+sqrt(1+8*n))/2.);
     }
 };
