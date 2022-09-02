5d20a4c0b46d61f6eb7c767a9a6c66ac660f4ac9
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-02 16:21:05 +0530

    Time: 64 ms (51.45%), Space: 14 MB (65.27%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -3,8 +3,23 @@ class Solution:
         m = min(arr)
         gap = (max(arr) - m) / (len(arr) - 1)
         if gap == 0: return True
-        s = set(num - m for num in arr)
-        return len(s) == len(arr) and all(diff % gap == 0 for diff in s)
+        i = 0
+        while i < len(arr):
+            if arr[i] == m + i * gap:
+                i += 1
+            else:
+                dis = arr[i] - m
+                if dis % gap != 0: return False
+                pos = int(dis / gap)
+                if arr[pos] == arr[i]: return False
+                arr[pos], arr[i] = arr[i], arr[pos]
+        return True
+        
+        # m = min(arr)
+        # gap = (max(arr) - m) / (len(arr) - 1)
+        # if gap == 0: return True
+        # s = set(num - m for num in arr)
+        # return len(s) == len(arr) and all(diff % gap == 0 for diff in s)
         
         # arr.sort()
         # d=arr[1]-arr[0]
