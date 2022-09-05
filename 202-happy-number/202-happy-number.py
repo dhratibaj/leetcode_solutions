634bb39bb5c8c2fa8337eb272924c118da349015
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-05 14:14:39 +0530

    Time: 83 ms (5.20%), Space: 13.8 MB (61.76%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,12 +1,28 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
-        while True:
-            if n in seen:
-                return False
-            elif n == 1:
-                return True
-            else:
-                seen.add(n)
-				# h(x): convert int to str -> sum(foreach character in str -> int(character) ^ 2) 
-                n = sum([int(x) ** 2 for x in str(n)])
+        def getNext(n):
+            tsum = 0
+            while n>0:
+                n,digit = divmod(n,10)
+                tsum += digit**2
+            return tsum
+        slow = n
+        fast = getNext(n)
+        while fast!=1 and slow!=fast:
+            slow = getNext(slow)
+            fast = getNext(getNext(fast))
+        return fast==1
+
+    # ---------------------2nd approach--------------------------------------
+        # def get_next(number):
+        #     total_sum = 0
+        #     while number > 0:
+        #         number, digit = divmod(number, 10)
+        #         total_sum += digit ** 2
+        #     return total_sum  
+        # while n != 1 and n != 4:
+        #     n = get_next(n) 
+        # return n == 1
+    
+    #-------------------------3rd brute force approach-------------------------------------------
+        
