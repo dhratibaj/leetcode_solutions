24904b192564cec58e6fe27c0c372a29a7d087e7
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-05 09:13:44 +0530

    Time: 77 ms (6.20%), Space: 14.2 MB (72.95%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -1,36 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> todo;
-        todo.push({root,{0,0}});
-        while(!todo.empty()){
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
-            int x =p.second.first, y=p.second.second;
-            nodes[x][y].insert(node->val);
-            if(node->left) todo.push({node->left,{x-1,y+1}});
-            if(node->right) todo.push({node->right,{x+1,y+1}});
-        }
-        vector<vector<int>> ans;
-        for(auto p: nodes){
-            vector<int> col;
-            for(auto q: p.second)
-                col.insert(col.end(),q.second.begin(),q.second.end());
-            ans.push_back(col);
-        }
-        return ans;
-    }
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        results = defaultdict(list)
+        queue = [ (root, 0, 0) ]
+        while queue:
+            node, pos, depth = queue.pop(0)
+            if not node: continue
+            results[(pos,depth)].append(node.val)
+            results[(pos,depth)].sort()
+            queue.extend( [ (node.left, pos-1, depth+1), (node.right, pos+1, depth+1) ] )
+        res = defaultdict(list)
+        keys = sorted(list(results.keys()), key=lambda x: (x[0], x[1]))
+        for k in keys:
+            pos, depth = k
+            res[pos].extend(results[k])
+        return res.values()
