4407dcbca14984a6137b3e1ee478994f56f1445f
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-07 11:14:50 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.03%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        transform(s.begin(),s.end(),s.begin(),::tolower);
+        for(int i=0;i<s.length();i++)
+            s[i] = tolower(s[i]);
         return s;
     }
 };
+
+// class Solution {
+// public:
+//     string toLowerCase(string s) {
+//         transform(s.begin(),s.end(),s.begin(),::tolower);
+//         return s;
+//     }
+// };
