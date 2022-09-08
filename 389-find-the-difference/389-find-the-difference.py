ca860a6d41177a81d525d29bda5376090bcdb8cf
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-08 23:00:03 +0530

    Time: 51 ms (63.40%), Space: 14 MB (31.79%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,18 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         d1,d2={},{}
-        for i in s:
+        for i in t:
             if i in d1:
                 d1[i] += 1
             else:
-                d1[i]=1
-        for i in t:
+                d1[i] = 1
+        for i in s:
             if i in d2:
                 d2[i] += 1
             else:
-                d2[i]=1
-        for i in d2:
-            if i in d1:
-                if d1[i] != d2[i]:
-                    return i
-        return i
+                d2[i] = 1
+        for i in d1:
+            if i not in d2 or d1[i] != d2[i]:
+                return i
