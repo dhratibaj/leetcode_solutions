24423d10e1917e1c582e1f644846ccc811a2ea14
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-09 12:02:55 +0530

    Time: 4 ms (38.31%), Space: 6.3 MB (82.77%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     string mergeAlternately(string w1, string w2) {
-        int n = w1.size(), m = w2.size(), i = 0, j = 0;
         string res = "";
-        while (i < n || j < m) {
-            if (i < n)
-                res.push_back(w1[i++]);
-            if (j < m)
-                res.push_back(w2[j++]);
+        for (int i = 0; i < w1.size() || i < w2.size(); ++i) {
+            if (i < w1.size())
+                res.push_back(w1[i]);
+            if (i < w2.size())
+                res.push_back(w2[i]);
         }
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     string mergeAlternately(string w1, string w2) {
+//         int n = w1.size(), m = w2.size(), i = 0, j = 0;
+//         string res = "";
+//         while (i < n || j < m) {
+//             if (i < n)
+//                 res.push_back(w1[i++]);
+//             if (j < m)
+//                 res.push_back(w2[j++]);
+//         }
+//         return res;
+//     }
+// };
