3ec5ed41ec54a08bb881f0d747032e2931bd15de
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-10 15:03:47 +0530

    Time: 62 ms (42.37%), Space: 13.8 MB (99.32%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -1,6 +1,15 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        return words == sorted(words, key=lambda word:[order.index(c) for c in word])
+        hm = {ch: i for i, ch in enumerate(order)}
+        prev_repr = list(hm[ch] for ch in words[0])
+        for i in range(1, len(words)):
+            cur_repr = list(hm[ch] for ch in words[i])
+            if cur_repr < prev_repr:
+                return False
+            prev_repr = cur_repr
+        return True
+        
+        # return words == sorted(words, key=lambda word:[order.index(c) for c in word])
         
         # d = {}
         # for i in range(len(order)):
