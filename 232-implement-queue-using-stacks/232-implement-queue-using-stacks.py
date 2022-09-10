1848c96a3a712c91cbd51069016213bd15997ca4
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-10 15:11:03 +0530

    Time: 55 ms (28.47%), Space: 14 MB (75.25%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -1,45 +1,22 @@
-class stack:
-    def __init__(self):
-        self.Stack = []
-    
-    def push(self,val):
-        self.Stack.append(val)
-    def pop(self):
-        return self.Stack.pop()
-    def isEmpty(self):
-        if len(self.Stack)==0:
-            return True
-        return False
-    
 class MyQueue:
 
     def __init__(self):
-        self.stacks = stack()
-        self.temp = stack()
-        
+        self.q = []
+
     def push(self, x: int) -> None:
-        self.stacks.push(x)
+        self.q.append(x)
         
     def pop(self) -> int:
-        if not self.temp.isEmpty():
-            return self.temp.pop()
-        else:
-            while not self.stacks.isEmpty():
-                self.temp.push(self.stacks.pop())
-            return self.temp.pop()
-        return self.stacks.pop()
-    
+        return self.q.pop(0)
+
     def peek(self) -> int:
-        if not self.temp.isEmpty():
-            return self.temp.Stack[-1]
-        else:
-            while not self.stacks.isEmpty():
-                self.temp.push(self.stacks.pop())
-            return self.temp.Stack[-1]
-        
+        return self.q[0]
+
     def empty(self) -> bool:
-        return self.stacks.isEmpty() and self.temp.isEmpty()
-    
+        if(self.q): return False
+        return True
+
+
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
