d42b9bdf663bd20c3983eda274f635bfcd4ab382
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-15 11:57:40 +0530

    Time: 24 ms (23.33%), Space: 10.1 MB (68.94%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,27 +1,49 @@
+// class Solution {
+// public:
+//     vector<int> sortByBits(vector<int>& arr) {
+        
+//     }
+// };
 class Solution {
 public:
-    int countsetbits(int n)
-    {
-        int c=0;
-        while(n)
-        {
-            if(n%2!=0)
-            c++;
-            n/=2;
-        }
-        return c;
+    static bool compare(const int& a, const int& b){
+        int c1 = __builtin_popcount(a);
+        int c2 = __builtin_popcount(b);
+        if(c1 == c2)
+            return a < b;
+        return c1 < c2;
     }
     vector<int> sortByBits(vector<int>& arr) {
-        map<int, vector<int>>mp;
-        vector<int>v;
-        sort(arr.begin(), arr.end());
-        for(int i=0; i<arr.size(); i++)
-        mp[countsetbits(arr[i])].push_back(arr[i]);
-        for(auto it=mp.begin(); it!=mp.end(); it++)
-        {
-            for(int a:it->second)
-            v.push_back(a);
-        }
-        return v;
+        std::sort(arr.begin(),arr.end(), compare);
+        return arr;
     }
 };
+
+
+// class Solution {
+// public:
+//     int countsetbits(int n)
+//     {
+//         int c=0;
+//         while(n)
+//         {
+//             if(n%2!=0)
+//             c++;
+//             n/=2;
+//         }
+//         return c;
+//     }
+//     vector<int> sortByBits(vector<int>& arr) {
+//         map<int, vector<int>>mp;
+//         vector<int>v;
+//         sort(arr.begin(), arr.end());
+//         for(int i=0; i<arr.size(); i++)
+//         mp[countsetbits(arr[i])].push_back(arr[i]);
+//         for(auto it=mp.begin(); it!=mp.end(); it++)
+//         {
+//             for(int a:it->second)
+//             v.push_back(a);
+//         }
+//         return v;
+//     }
+// };
