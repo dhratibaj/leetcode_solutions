023b144dd92c8bf4dd62b16845e3bb9d1c6b0484
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2022-09-15 11:59:03 +0530

    Time: 28 ms (15.42%), Space: 10.3 MB (45.06%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,23 +1,32 @@
-// class Solution {
-// public:
-//     vector<int> sortByBits(vector<int>& arr) {
-        
-//     }
-// };
 class Solution {
 public:
-    static bool compare(const int& a, const int& b){
-        int c1 = __builtin_popcount(a);
-        int c2 = __builtin_popcount(b);
-        if(c1 == c2)
-            return a < b;
-        return c1 < c2;
+    static bool compare(int a, int b){
+        bitset<32> b1(a);
+        bitset<32> b2(b);
+        if(b1.count() != b2.count()){
+            return b1.count() < b2.count();
+        }
+        else  return a < b;
     }
     vector<int> sortByBits(vector<int>& arr) {
-        std::sort(arr.begin(),arr.end(), compare);
+        std::sort(arr.begin(), arr.end(), compare);
         return arr;
     }
 };
+// class Solution {
+// public:
+//     static bool compare(const int& a, const int& b){
+//         int c1 = __builtin_popcount(a);
+//         int c2 = __builtin_popcount(b);
+//         if(c1 == c2)
+//             return a < b;
+//         return c1 < c2;
+//     }
+//     vector<int> sortByBits(vector<int>& arr) {
+//         std::sort(arr.begin(),arr.end(), compare);
+//         return arr;
+//     }
+// };
 
 
 // class Solution {
